55a66a5cd6be49637e3c3d05aef25509e84cf906
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2022-01-27 17:51:11 +0300

    Initial

---
+++ Constants.cs
@@ -0,0 +1,11 @@
+﻿class Weapon
+{
+    private int _bullets;
+    private const int _minimalAmmo = 0;
+    private const int _bulletsRate = 1;
+    
+
+    public bool CanShoot() => _bullets > _minimalAmmo;
+
+    public void Shoot() => _bullets -= _bulletsRate;
+}
